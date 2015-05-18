0d2082b6e5d8b1f9782d93c6995b2317c34b3f6e
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2015-05-18 16:49:45 +0200

    Cleaning/improving vector

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,9 +1,11 @@
 #include <vector>
+#include <iostream>
 
 struct S {};
 
 int main() {
 	std::vector<S*> vv;
 	vv.push_back(0);
+	std::cout << "program is ending and vector size is : " << vv.size() << std::endl;
 }
 
